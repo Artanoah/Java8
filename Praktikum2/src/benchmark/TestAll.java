8ac4e071e9bc66366d10e043eef4fa0ff6982642
Artanoah <artanoah@web.de>
2014-11-11 13:10:06 +0100

    Dinge

--- Praktikum2/src/benchmark/TestAll.java
+++ Praktikum2/src/benchmark/TestAll.java
@@ -1,6 +1,7 @@
 package benchmark;
 
 import java.lang.reflect.Method;
+import java.lang.management.*;
 import java.util.Scanner;
 
 import measure.MethodsTimer;
@@ -10,13 +11,21 @@ import streamsVsForEach.StreamTestObject;
 public class TestAll {
 
 	public static void main(String[] args) throws Exception {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.next();
-		
-		Method[] methods = new Method[]{ForEachTestObject.class.getMethod("forEachBenchmark", new Class[0]),
+
+		/*System.out.println("###### Zeit in Microsekunden ######");
+ 		Method[] methods = new Method[]{ForEachTestObject.class.getMethod("forEachBenchmark", new Class[0]),
 				StreamTestObject.class.getMethod("streamBenchmark", new Class[0])};
 		
 		MethodsTimer mt = new MethodsTimer(methods);
 		mt.report();
+		*/
+		
+		System.out.println("###### Speicherbedarf ######");
+		
+		Runtime runtime = Runtime.getRuntime();
+		ThreadMXBean mmxb = ManagementFactory.getThreadMXBean();
+		
+		runtime.totalMemory();
+		
 	}
 }
