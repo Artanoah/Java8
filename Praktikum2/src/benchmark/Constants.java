92b3654038a2aca63c649ac00a344459bf075023
Artanoah <artanoah@web.de>
2014-11-19 13:49:03 +0100

    Dinge

---
+++ Praktikum2/src/benchmark/Constants.java
@@ -0,0 +1,5 @@
+package benchmark;
+
+public class Constants {
+	public static final String sgbWords = "sgb-words.txt";
+}
--- Praktikum2/src/streamsVsForEach/ForEachTestObject.java
+++ Praktikum2/src/streamsVsForEach/ForEachTestObject.java
@@ -1,15 +1,23 @@
 package streamsVsForEach;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ForEachTestObject {
-	private List<Integer> li = new ArrayList<Integer>();
+import benchmark.Constants;
 
+
+public class ForEachTestObject {
+//	private List<Integer> li = new ArrayList<Integer>();
+	private List<String> li = new ArrayList<String>();
 	
-	public ForEachTestObject(int size) {
-		for(int i = 0; i < size; i++) {
-			li.add((int) Math.random() * 1000);
+	public ForEachTestObject(int size) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(Constants.sgbWords));
+		
+		while(br.ready()) {
+			li.add(br.readLine());
 		}
 	}
 	
