ff82bdc55583947de6a11bf31c578fb30d0959f9
Hector Smith <hector.smith@haw-hamburg.de>
2014-12-05 12:22:51 +0100

    Improved implementation of the InputDataReader. It now loops back to the start of the file when EOF is reached.

--- Praktikum2-jmh/jmh-benchmarks/src/main/java/io_manager/InputDataReader.java
+++ Praktikum2-jmh/jmh-benchmarks/src/main/java/io_manager/InputDataReader.java
@@ -10,13 +10,35 @@ public class InputDataReader {
 
 	public static List<String> readFileLines(String inputFile, int numberOfLines) throws IOException {
 		
-		List<String> strList = new ArrayList<>();
+		if (numberOfLines <= 0) {
+			throw new IllegalArgumentException("Invalid number of lines: " + numberOfLines);
+		}
+		if (inputFile == null || inputFile.isEmpty()) {
+			throw new IllegalArgumentException("Invalid input file: " + inputFile);
+		}
 		
+		List<String> strList = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
 		
 		String line;
-		while ((line = reader.readLine()) != null) {
-			strList.add(line);
+		for (int i = 0; i < numberOfLines; i++) {
+			//	Read line
+			line = reader.readLine();
+			
+			//	Test for EOF
+			if (line == null) {
+				//	Reset reader to file start
+				reader.close();
+				reader = new BufferedReader(new FileReader(inputFile));
+				
+				//	Read new line
+				line = reader.readLine();
+				if (line == null) {
+					reader.close();
+					throw new IOException("Problems reading file: " + inputFile);
+				}
+			}
+			strList.add(line);			
 		}
 		
 		reader.close();
