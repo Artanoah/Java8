86182043f1fcd296e5c06d09770e20ddfe8d71b9
Hector Smith <hector.smith@haw-hamburg.de>
2014-12-05 11:43:09 +0100

    Updated classes and added notations.

--- Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/streamsVsForEach/ForEachTestObject.java
+++ Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/streamsVsForEach/ForEachTestObject.java
@@ -1,21 +1,27 @@
 package benchmarks.streamsVsForEach;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import general.Constants;
 import io_manager.InputDataReader;
+
 import java.io.IOException;
 import java.util.List;
 
-import general.Constants;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
-
+@State(Scope.Benchmark)
 public class ForEachTestObject {
 
-        @Benchmark
+    @Param({"10", "100", "200", "400", "800"})
+	public int size;
+	
+	@Benchmark
 	public void forEachBenchmark() throws IOException {
-                int size = 100;
-                List<String> list = InputDataReader.readFileLines(Constants.sgbWords, size);
+		List<String> list = InputDataReader.readFileLines(Constants.sgbWords, size);
 
-		for(String element : list) {
+		for (String element : list) {
 			element.toUpperCase();
 		}
 	}
--- Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/streamsVsForEach/StreamTestObject.java
+++ Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/streamsVsForEach/StreamTestObject.java
@@ -1,19 +1,26 @@
 package benchmarks.streamsVsForEach;
 
-import io_manager.InputDataReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import general.Constants;
+import io_manager.InputDataReader;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+
+@State(Scope.Benchmark)
 public class StreamTestObject {
-	private List<String> list = new ArrayList<>();
+
+    @Param({"10", "100", "200", "400", "800"})
+	public int size;
 	
-	public StreamTestObject(int size) throws IOException {
-		list = InputDataReader.readFileLines(Constants.sgbWords, size);
-	}
-	
-	public void streamBenchmark() {
+	@Benchmark
+	public void streamBenchmark() throws IOException {	
+		List<String> list = InputDataReader.readFileLines(Constants.sgbWords, size);
 		list.stream().forEach(e -> e.toUpperCase());
 	}
 
