52644e2d76a250f9bdc0bdfe8aa3711519311d02
Artanoah <artanoah@web.de>
2014-12-07 16:34:43 +0100

    JavaDoc

--- Praktikum2-ownBenchmark/src/io_manager/InputDataReader.java
+++ Praktikum2-ownBenchmark/src/io_manager/InputDataReader.java
@@ -6,9 +6,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Hilfsklasse fuer das Auslesen einer benoetigten 
+ * Anzahl an Strings aus der Datei "sgb-words" fuer 
+ * das Benchmarken der Testobjekte.
+ */
 public class InputDataReader {
 
-public static List<String> readFileLines(String inputFile, int numberOfLines) throws IOException {
+	/**
+	 * Liesst <code>numberOfLines</code> Strings ein und gibt 
+	 * diese als <code>List&lt;String&gt;</code> zurueck. Wenn 
+	 * die Datei nicht genuegend Zeilen hat, wird die Datei 
+	 * wieder von vorne gelesen.
+	 * 
+	 * @param inputFile Datei aus der die Strings ausgelesen werden.
+	 * @param numberOfLines Anzahl der Zeilen die ausgelesen werden 
+	 * sollen.
+	 * @return Liste mit den benoetigten Strings
+	 * @throws IOException Tritt bei Fehlern beim Oeffnen oder Lesen 
+	 * der Datei <code>inputFile</code> auf.
+	 */
+	public static List<String> readFileLines(String inputFile, int numberOfLines) throws IOException {
 		
 		if (numberOfLines <= 0) {
 			throw new IllegalArgumentException("Invalid number of lines: " + numberOfLines);
