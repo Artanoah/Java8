ddbea0d115882c26e570a5d096eb4cc4c8b7ea7a
Jeremias <jeremias.twele@haw-hamburg.de@home.de>
2014-12-07 17:22:36 +0100

    added javadoc

--- Praktikum2-Codeabgabe/Praktikum2-ownBenchmark/src/LambdaVSAnonymous/LambdaTestObject.java
+++ Praktikum2-Codeabgabe/Praktikum2-ownBenchmark/src/LambdaVSAnonymous/LambdaTestObject.java
@@ -14,10 +14,23 @@ public class LambdaTestObject {
 
 	private List<String> list = new ArrayList<>();
 
+	/**
+	 * Constructor
+	 * The Constructor initializes the list and fills it
+	 * with the given number of lines from the Constants.sgbWords
+	 * 
+	 * @param size The number of lines to be read
+	 * @throws IOException
+	 */
 	public LambdaTestObject(int size) throws IOException {
 		list = InputDataReader.readFileLines(Constants.sgbWords, size);
 	}
 
+	
+	/**
+	 * Iterates through the list using forEach and call toUpperCase()
+	 * on each element using LambdaExpression
+	 */
 	public void lambdaBenchmark() {
 		list.forEach(e -> e.toUpperCase());
 	}
