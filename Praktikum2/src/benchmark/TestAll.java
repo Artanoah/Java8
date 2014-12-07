687c1371413d69bb83a360a696bc3ebaf8cd6415
Max Zender <mail@maxzender.de>
2014-12-07 13:06:09 +0100

    add benchmark for lambdas vs anonymous classes

--- Praktikum2/src/benchmark/TestAll.java
+++ Praktikum2/src/benchmark/TestAll.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Method;
 import java.lang.management.*;
 import java.util.Scanner;
 
+import LambdaVSAnonymous.AnonymousClassTestObject;
+import LambdaVSAnonymous.LambdaTestObject;
 import LambdaVSReflection.LambdaRefTestObject;
 import LambdaVSReflection.ReflectionLamTestObject;
 import parallelStreamsVSSerialStreams.ParallelStreamTestObject;
@@ -19,12 +21,14 @@ public class TestAll {
 		Method[] methods = new Method[]{
 				ReflectionLamTestObject.class.getMethod("reflectionBenchmark", new Class[0]),
 				LambdaRefTestObject.class.getMethod("lambdaBenchmark", new Class[0]),
-				
+
 				ForEachTestObject.class.getMethod("forEachBenchmark", new Class[0]),
 				StreamTestObject.class.getMethod("streamBenchmark", new Class[0]),
-				
+
 				SerialStreamTestObject.class.getMethod("serialStreamBenchmark", new Class[0]),
-				ParallelStreamTestObject.class.getMethod("parallelStreamBenchmark", new Class[0])
+				ParallelStreamTestObject.class.getMethod("parallelStreamBenchmark", new Class[0]),
+				AnonymousClassTestObject.class.getMethod("anonymousClassBenchmark", new Class[0]),
+				LambdaTestObject.class.getMethod("lambdaBenchmark", new Class[0])
 				};
 
 		MethodsTimer mt = new MethodsTimer(methods);
