4017646d30377826a9a46a8d8cabde5c0e0b1e19
Hendrik <mail@hendrikalbers.de>
2015-01-11 15:27:28 +0100

    parallel vs serial stream: read input list in @setup method

--- Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/parallelStreamsVSSerialStreams/ParallelStreamTestObject.java
+++ Praktikum2-jmh/jmh-benchmarks/src/main/java/benchmarks/parallelStreamsVSSerialStreams/ParallelStreamTestObject.java
@@ -11,28 +11,37 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Level;
 
 /**
  * @author Hendrik Albers, Steffen Giersch, Maximilian Heinrich, Hector Smith, Jeremias Twele
  */
 
-@State(Scope.Benchmark)
+@State(Scope.Thread)
 public class ParallelStreamTestObject {
 
 	/**
 	 * size is the number of lines to be read
 	 * The jmh benchmark will run a test for each value
 	 */
-    @Param({ "100", "1000", "10000", "100000", "1000000", "10000000" })
+    @Param({ "1000", "10000", "100000" })
 	public int size;
+	
+	List<String> list;
+	
+	@Setup(Level.Iteration)
+	public void initializeList() throws IOException {
+		list = InputDataReader.readFileLines(Constants.sgbWords, size);
+		
+	}
 	
 	/**
 	 * Iterates through the list using forEach and call toUpperCase()
 	 * on each element using parallelStream
 	 */
 	@Benchmark
-	public List<String> parallelStreamBenchmark() throws IOException {
-		List<String> list = InputDataReader.readFileLines(Constants.sgbWords, size);
+	public List<String> parallelStreamBenchmark() {
 		List<String> result = new ArrayList<>(size);
 		
 		list.parallelStream().forEach(e -> result.add(e.toUpperCase()));
